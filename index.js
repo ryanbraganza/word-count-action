4161c24138f726be327f41744c9b630ae3e6cca3
Ryan Braganza <ryan.braganza@gmail.com>
2020-03-28 21:26:09 +1100

    rm console.log

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const github = require('@actions/github');
 
 try {
   const words = core.getInput('words');
-  console.log(`Hello ${nameToGreet}!`);
   const count = words.trim().split(/\s+/).length;
   core.setOutput('count', count);
   // Get the JSON webhook payload for the event that triggered the workflow
