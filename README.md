497f519e4d5779159f91fd187c958afba15fa3a6
Ryan Braganza <ryan.braganza@gmail.com>
2020-03-28 21:10:33 +1100

    v1

---
+++ README.md
@@ -0,0 +1,21 @@
+# Word Count JavaScript action
+
+This action counts the number of words provided.
+
+## Inputs
+
+### `words`
+
+**Required** The string of words to count. Default `"one two three"`
+
+## Outputs
+
+### `count`
+
+The number of words
+
+## Example usage
+
+uses: ryanbraganza/word-count-action@v1
+with:
+  words: 'Stay inside. Do not go out.'
---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const words = core.getInput('words');
+  console.log(`Hello ${nameToGreet}!`);
+  const count = words.trim().split(/\s+/).length;
+  core.setOutput('count', count);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
