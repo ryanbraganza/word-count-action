9d56f9b9ba5fef91a3ed0c7676245f5a39b8116a
Ryan Braganza <ryan.braganza@gmail.com>
2020-03-28 21:36:03 +1100

    v4

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ try {
   const words = core.getInput('words');
   const count = words.split(/\s+/).length;
   core.setOutput('count', count);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
